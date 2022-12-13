57d0e5db5c90041ffabb533efb315d2facb05a29
Ilya <ilya_mukosey@gmail.com>
2022-12-13 18:34:30 +0300

    intro-mongoDB

---
+++ 4-natours/starter/routes/tourRoutes.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const tourController = require('../controllers/tourController')
+
+const {getAllTours, createTour, getTour, updateTour,deleteTour, checkID, checkTour} = tourController;
+
+const router = express.Router();
+
+router.param('id', checkID);
+
+router
+    .route('/')
+    .get(getAllTours)
+    .post(checkTour, createTour);
+
+router
+    .route('/:id') 
+    .get(getTour)
+    .patch(updateTour)
+    .delete(deleteTour)   
+
+
+module.exports = router;    
